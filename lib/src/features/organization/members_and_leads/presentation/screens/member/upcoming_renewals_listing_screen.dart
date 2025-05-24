0eab7d4cd8b3121318ce345eface1d740018d038
salahu01 <cvsalahu12@gmail.com>
2025-05-24 20:52:11 +0530

    feat: refactor upcoming renewals listing screen layout and improve member display logic

--- lib/src/features/organization/members_and_leads/presentation/screens/member/upcoming_renewals_listing_screen.dart
+++ lib/src/features/organization/members_and_leads/presentation/screens/member/upcoming_renewals_listing_screen.dart
@@ -61,121 +61,97 @@ class __UpcomingRenewalsListingScreenState extends State<_UpcomingRenewalsListin
           Expanded(
             child: ColoredBox(
               color: const Color(0xffF7F7F7),
-              child: Column(
-                spacing: 16,
-                children: [
-                  SizedBox(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text('88 members', style: AppStyles.text14Px.poppins.w400.dark),
-                        OutlinedButton.icon(
-                          style: OutlinedButton.styleFrom(padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 6), side: const BorderSide(color: Color(0xffDDDDDD))),
-                          onPressed: () {},
-                          icon: Text('Sort by', style: AppStyles.text12Px.poppins.copyWith(color: const Color(0xff222222))),
-                          label: const Icon(Icons.keyboard_arrow_down_sharp),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Expanded(
-                    child: ColoredBox(
-                      color: const Color(0xffF7F7F7),
-                      child: BlocBuilder<MembersAndLeadsCubit, MembersAndLeadsState>(
-                        buildWhen: (p, c) => p.members != c.members,
-                        builder: (context, state) {
-                          return state.members.data.fold(
-                            () => const Center(child: CircularProgressIndicator()),
-                            (either) => either.fold((error) => const Center(child: Text('Error')), (memebersDataum) {
-                              if (memebersDataum.results?.isEmpty ?? true) {
-                                return const Center(child: Text('No members found'));
-                              }
-                              return Column(
-                                children: [
-                                  Row(
-                                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                    children: [
-                                      Text('${memebersDataum.count ?? 0} members', style: AppStyles.text14Px.poppins.w400.dark),
-                                      OutlinedButton(
-                                        style: OutlinedButton.styleFrom(padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 6), side: const BorderSide(color: Color(0xffDDDDDD))),
-                                        onPressed: () {},
-                                        child: Text('Sort by Joined Recently', style: AppStyles.text12Px.poppins.copyWith(color: const Color(0xff222222))),
-                                      ),
-                                    ],
-                                  ),
-                                  const SizedBox(height: 16),
-                                  Expanded(
-                                    child: RefreshIndicator.adaptive(
-                                      onRefresh: _fetch,
-                                      child: ListView.separated(
-                                        controller: _scrollController,
-                                        itemCount: memebersDataum.results?.length ?? 0,
-                                        separatorBuilder: (BuildContext context, int index) {
-                                          return const SizedBox(height: 16);
-                                        },
-                                        itemBuilder: (BuildContext context, int index) {
-                                          final memberData = memebersDataum.results?[index];
-                                          return ClipRRect(
-                                            borderRadius: BorderRadius.circular(16),
-                                            child: ColoredBox(
-                                              color: Colors.white,
-                                              child: Padding(
-                                                padding: const EdgeInsets.all(16),
-                                                child: Column(
-                                                  spacing: 8,
-                                                  children: [
-                                                    Row(
+              child: BlocBuilder<MembersAndLeadsCubit, MembersAndLeadsState>(
+                buildWhen: (p, c) => p.members != c.members,
+                builder: (context, state) {
+                  return state.members.data.fold(
+                    () => const Center(child: CircularProgressIndicator()),
+                    (either) => either.fold((error) => const Center(child: Text('Error')), (memebersDataum) {
+                      if (memebersDataum.results?.isEmpty ?? true) {
+                        return const Center(child: Text('No members found'));
+                      }
+                      return Column(
+                        spacing: 16,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text('${memebersDataum.count ?? 0} members', style: AppStyles.text14Px.poppins.w400.dark),
+                              OutlinedButton(
+                                style: OutlinedButton.styleFrom(padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 6), side: const BorderSide(color: Color(0xffDDDDDD))),
+                                onPressed: () {},
+                                child: Text('Sort by', style: AppStyles.text12Px.poppins.copyWith(color: const Color(0xff222222))),
+                              ),
+                            ],
+                          ),
+                          Expanded(
+                            child: RefreshIndicator.adaptive(
+                              onRefresh: _fetch,
+                              child: ListView.separated(
+                                controller: _scrollController,
+                                itemCount: memebersDataum.results?.length ?? 0,
+                                separatorBuilder: (BuildContext context, int index) {
+                                  return const SizedBox(height: 16);
+                                },
+                                itemBuilder: (BuildContext context, int index) {
+                                  final memberData = memebersDataum.results?[index];
+                                  return ClipRRect(
+                                    borderRadius: BorderRadius.circular(16),
+                                    child: ColoredBox(
+                                      color: Colors.white,
+                                      child: Padding(
+                                        padding: const EdgeInsets.all(16),
+                                        child: Column(
+                                          spacing: 8,
+                                          children: [
+                                            Row(
+                                              crossAxisAlignment: CrossAxisAlignment.start,
+                                              children: [
+                                                ClipRRect(
+                                                  borderRadius: const BorderRadius.all(Radius.circular(80000)),
+                                                  child: ProfileImage(isEdit: false, url: '${memberData?.profilePicture ?? ''}', radius: 40),
+                                                ),
+                                                const SizedBox(width: 8),
+                                                Flexible(
+                                                  child: SizedBox(
+                                                    width: double.maxFinite,
+                                                    child: Column(
                                                       crossAxisAlignment: CrossAxisAlignment.start,
                                                       children: [
-                                                        ClipRRect(
-                                                          borderRadius: const BorderRadius.all(Radius.circular(80000)),
-                                                          child: ProfileImage(isEdit: false, url: '${memberData?.profilePicture ?? ''}', radius: 40),
-                                                        ),
-                                                        const SizedBox(width: 8),
-                                                        Flexible(
-                                                          child: SizedBox(
-                                                            width: double.maxFinite,
-                                                            child: Column(
-                                                              crossAxisAlignment: CrossAxisAlignment.start,
-                                                              children: [
-                                                                Text(memberData?.name ?? '', style: AppStyles.text14Px.poppins.w500.dark),
-                                                                const SizedBox(height: 4),
-                                                                Text(memberData?.mobileNumber ?? '', style: AppStyles.text12Px.poppins.w400.dark),
-                                                              ],
-                                                            ),
-                                                          ),
-                                                        ),
-                                                        const SizedBox(width: 8),
-                                                        Text('memberId', style: AppStyles.text12Px.poppins.w400.textGrey),
-                                                      ],
-                                                    ),
-                                                    Text('Check In', style: AppStyles.text10Px.poppins.w400.textGrey, textAlign: TextAlign.end).align(Alignment.centerRight),
-                                                    Row(
-                                                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                                      children: [
-                                                        Text(memberData?.activePlan?.planName ?? '', style: AppStyles.text12Px.poppins.w500.dark),
+                                                        Text(memberData?.name ?? '', style: AppStyles.text14Px.poppins.w500.dark),
                                                         const SizedBox(height: 4),
-                                                        Text('05:30 AM', style: AppStyles.text12Px.poppins.w500.dark),
+                                                        Text(memberData?.mobileNumber ?? '', style: AppStyles.text12Px.poppins.w400.dark),
                                                       ],
                                                     ),
-                                                  ],
+                                                  ),
                                                 ),
-                                              ),
+                                                const SizedBox(width: 8),
+                                                Text('memberId', style: AppStyles.text12Px.poppins.w400.textGrey),
+                                              ],
                                             ),
-                                          );
-                                        },
+                                            Text('Check In', style: AppStyles.text10Px.poppins.w400.textGrey, textAlign: TextAlign.end).align(Alignment.centerRight),
+                                            Row(
+                                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                              children: [
+                                                Text(memberData?.activePlan?.planName ?? '', style: AppStyles.text12Px.poppins.w500.dark),
+                                                const SizedBox(height: 4),
+                                                Text('05:30 AM', style: AppStyles.text12Px.poppins.w500.dark),
+                                              ],
+                                            ),
+                                          ],
+                                        ),
                                       ),
                                     ),
-                                  ),
-                                ],
-                              ).pad(16);
-                            }),
-                          );
-                        },
-                      ),
-                    ),
-                  ),
-                ],
+                                  );
+                                },
+                              ),
+                            ),
+                          ),
+                        ],
+                      ).pad(16);
+                    }),
+                  );
+                },
               ).pad(16),
             ),
           ),
