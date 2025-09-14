48242d757184b7674b38db52fdafe1ae8526d7ca
kpmidhlaj <midhlajkp@cookee.io>
2025-09-14 10:31:17 +0530

    Added new auth repo flow

--- lib/src/features/auth/login/domain/repositories/auth_repository.dart
+++ lib/src/features/auth/login/domain/repositories/auth_repository.dart
@@ -1,65 +1,56 @@
 import 'dart:developer' as log;
 
+import 'package:mentor_mobile_app/core/network/dio_client.dart';
 import 'package:mentor_mobile_app/imports_bindings.dart';
 
 @immutable
 final class AuthRepository {
   ///* This constructor body for creating singleton widget
   factory AuthRepository() {
-    _instance ??= const AuthRepository._internal();
+    _instance ??= AuthRepository._internal();
     return _instance!;
   }
 
   //* This named constructor for create object for this class
-  const AuthRepository._internal();
+  AuthRepository._internal();
 
   //* This variable for store this class object globally
   static AuthRepository? _instance;
+
+  final Dio _dio = DioClient().dio;
 
   /*
    @api {POST https://discipl-backend.onrender.com/api/v1/user/send-otp} https://discipl-backend.onrender.com/api/v1/user/send-otp
    @apiName sent_otp
    @apiGroup Auth
-
-   @apiBody {json} body Request payload
-   ```json
-   {
-    "mobile_number": "+917510661748",
-    "process": "login",
-    "source": "mentor-app-android"
-}
-   ```
-
-   @apiSuccess {SentOtpEntity} response Success response
    */
   Future<Either<ApiException, SentOtpEntity>> sentOtp({
     required Map<String, dynamic> body,
   }) async {
     try {
       return await Feggy.async(
-        call: Dio().post<dynamic>(
+        call: _dio.post<dynamic>(
           ApiUris.sentOtp,
           data: body,
           options: Options(headers: {'X-Platform': platformSource}),
         ),
         onSuccess: (res) {
-          log.log(res.toString(), name: 'AuthRepository');
-          if ([200, 201].contains(res.statusCode)) {
-            if (res.data != null && res.data is Map) {
-              return right(
-                SentOtpEntity.fromJson(res.data as Map<String, dynamic>),
-              );
-            }
+          log.log(res.toString(), name: 'AuthRepository.sentOtp');
+          if ([200, 201].contains(res.statusCode) &&
+              res.data != null &&
+              res.data is Map) {
+            return right(
+              SentOtpEntity.fromJson(res.data as Map<String, dynamic>),
+            );
           }
-          log.log(res.toString(), name: 'AuthRepository');
           return left(const ApiException.unknown());
         },
       );
     } on ApiException catch (e) {
-      log.log(e.toString(), name: 'AuthRepository');
+      log.log(e.toString(), name: 'AuthRepository.sentOtp');
       return left(e);
     } catch (e) {
-      log.log(e.toString(), name: 'AuthRepository');
+      log.log(e.toString(), name: 'AuthRepository.sentOtp');
       return left(const ApiException.unknown());
     }
   }
@@ -68,40 +59,23 @@ final class AuthRepository {
    @api {POST https://discipl-backend.onrender.com/api/v1/user/otp/verify} https://discipl-backend.onrender.com/api/v1/user/otp/verify
    @apiName login_with_otp
    @apiGroup Auth
-
-   @apiBody {json} body Request payload
-   ```json
-   {
-    "process": "login",
-    "source": "mentor-app-ios",
-    "otp_id": "{{AUTHORIZATION_OTP_ID}}",
-    "otp": "{{AUTHORIZATION_OTP_CODE}}",
-    "mobile_number": "+917510661748"
-}
-   ```
-
-   @apiSuccess {LoginWithOtpEntity} response Success response
    */
   Future<Either<ApiException, LoginSuccessModel>> loginWithOtp({
     required Map<String, dynamic> body,
   }) async {
     try {
       return await Feggy.async(
-        call: Dio().post<dynamic>(
+        call: _dio.post<dynamic>(
           ApiUris.loginWithOtp,
           data: body,
           options: Options(headers: {'X-Platform': platformSource}),
         ),
-        customHandler: (error) {
-          return null;
-        },
+        customHandler: (error) => null,
         onSuccess: (res) {
-          if (res.statusCode == 200) {
-            if (res.data != null && res.data is Map) {
-              return right(
-                LoginSuccessModel.fromJson(res.data as Map<String, dynamic>),
-              );
-            }
+          if (res.statusCode == 200 && res.data != null && res.data is Map) {
+            return right(
+              LoginSuccessModel.fromJson(res.data as Map<String, dynamic>),
+            );
           }
           return left(const ApiException.unknown());
         },
@@ -117,35 +91,20 @@ final class AuthRepository {
    @api {POST https://discipl-backend.onrender.com/api/v1/user/otp/verification/registration} https://discipl-backend.onrender.com/api/v1/user/otp/verification/registration
    @apiName verify_otp
    @apiGroup Auth
-
-   @apiBody {json} body Request payload
-   ```json
-   {
-    "process": "registration",
-    "source": "mentor-app-android",
-    "otp_id": "{{AUTHORIZATION_OTP_ID}}",
-    "otp": "{{AUTHORIZATION_OTP_CODE}}",
-    "mobile_number": "+917510661748"
-}
-   ```
-
-   @apiSuccess {VerifyOtpEntity} response Success response
    */
   Future<Either<ApiException, void>> verifyOtp({
     required Map<String, dynamic> body,
   }) async {
     try {
       return await Feggy.async(
-        call: Dio().post<dynamic>(
+        call: _dio.post<dynamic>(
           ApiUris.verifyOtp,
           data: body,
           options: Options(headers: {'X-Platform': platformSource}),
         ),
         onSuccess: (res) {
-          if (res.statusCode == 200) {
-            if (res.data != null && res.data is Map) {
-              return right(null);
-            }
+          if (res.statusCode == 200 && res.data != null && res.data is Map) {
+            return right(null);
           }
           return left(const ApiException.unknown());
         },
@@ -161,43 +120,24 @@ final class AuthRepository {
    @api {POST https://discipl-backend.onrender.com/api/v1/user/onboarding} https://discipl-backend.onrender.com/api/v1/user/onboarding
    @apiName onboarding
    @apiGroup Auth
-
-   @apiBody {json} body Request payload
-   ```json
-   {
-    "email": "kasyap@gamail.com",
-    "first_name": "kumari",
-    "last_name": "k",
-    "otp_id": "{{AUTHORIZATION_OTP_ID}}",
-    "mobile_number": "+917510661748",
-    "process": "registration",
-    "source": "mentor-app-android",
-    "user_role": "20",
-    "meta":{
-
-    }
-}
-  ```
-
-  @apiSuccess {OnboardingEntity} response Success response
    */
   Future<Either<ApiException, LoginSuccessModel>> onboarding({
     required Map<String, dynamic> body,
   }) async {
     try {
       return await Feggy.async(
-        call: Dio().post<dynamic>(
+        call: _dio.post<dynamic>(
           ApiUris.onboarding,
           data: body,
           options: Options(headers: {'X-Platform': platformSource}),
         ),
         onSuccess: (res) {
-          if ([200, 201].contains(res.statusCode)) {
-            if (res.data != null && res.data is Map) {
-              return right(
-                LoginSuccessModel.fromJson(res.data as Map<String, dynamic>),
-              );
-            }
+          if ([200, 201].contains(res.statusCode) &&
+              res.data != null &&
+              res.data is Map) {
+            return right(
+              LoginSuccessModel.fromJson(res.data as Map<String, dynamic>),
+            );
           }
           return left(const ApiException.unknown());
         },
@@ -213,21 +153,17 @@ final class AuthRepository {
    @api {POST https://discipl-backend.onrender.com/api/v1/user/logout} https://discipl-backend.onrender.com/api/v1/user/logout
    @apiName logout
    @apiGroup Auth
-
-   @apiSuccess {LogoutModel} response Success response
    */
   Future<Either<ApiException, void>> logout() async {
     try {
       return await Feggy.async(
-        call: Dio().post<dynamic>(
+        call: _dio.post<dynamic>(
           ApiUris.logout,
           options: Options(headers: {'X-Platform': platformSource}).token,
         ),
         onSuccess: (res) {
-          if (res.statusCode == 200) {
-            if (res.data != null && res.data is Map) {
-              return right(null);
-            }
+          if (res.statusCode == 200 && res.data != null && res.data is Map) {
+            return right(null);
           }
           return left(const ApiException.unknown());
         },
